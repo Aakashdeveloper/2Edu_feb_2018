77eeedab63138654ee8b10b585e15a617674f2bb
Aakashdeveloper <aakash.h@hcl.com>
2018-02-08 19:18:33 +0530

    A

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
 import {AppComponent} from './app.component'
 import {CarComponent} from './car.component';
@@ -8,7 +9,8 @@ import { ProductComponent } from './products/products.component';
 
 @NgModule({
     imports:[
-        BrowserModule
+        BrowserModule,
+        FormsModule
     ],
     declarations:[
         AppComponent,
